830d0d1f2abb7f58ddf2a7135a892fe905c09c83
wintttr <wintttergewitter@gmail.com>
2023-07-10 18:56:07 +0300

    Добавил класс для чтения Excel файла

---
+++ ExcelReader.cs
@@ -0,0 +1,42 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    class ExcelReader
+    {
+        private string _filePath;
+
+        public ExcelReader(string path)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            _filePath = path;
+        }
+
+        private string GetPlainText(ExcelWorksheet ws, string address)
+        {
+            StringBuilder result = new(ws.Cells[address].GetValue<string>());
+            result.Replace("_x000d_", null);
+            return result.ToString().Trim();
+        }
+
+        public TeachPlan GetPlan()
+        {
+            string trainDir, prof, qual;
+            using (var package = new ExcelPackage(new FileInfo(_filePath)))
+            {
+                var ws = package.Workbook.Worksheets.First(t => t.Name == "Титул");
+                trainDir = GetPlainText(ws, "D29");
+                prof = GetPlainText(ws, "D30");
+                qual = GetPlainText(ws, "C40");
+            }
+
+            return new(trainDir, prof, qual); 
+        }
+    }
+}
