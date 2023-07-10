9871976ca4d894d7dec31d8a7198e2cfde3dafa1
wintttr <wintttergewitter@gmail.com>
2023-07-10 15:35:35 +0300

    Добавил класс учебного плана

---
+++ TeachPlan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    public class TeachPlan
+    {
+        public string Name { get; init; }
+        public string TrainingDirection { get; init; }
+        public string Profile { get; init; }
+        public string Qualification { get; init; }
+    }
+}
