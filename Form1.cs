38979522044b9a51fe40f2f3fec87333a60d9d92
wintttr <wintttergewitter@gmail.com>
2023-07-10 21:57:36 +0300

    Добавил диалог открытия файла

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
@@ -5,6 +7,26 @@ namespace WinFormsApp1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using OpenFileDialog ofd = new();
+
+            ofd.CheckFileExists = true;
+            ofd.CheckPathExists = true;
+            ofd.DefaultExt = "xlsx";
+            ofd.Filter = "Excel files (*.xlsx)|*.xlsx|All files(*.*)|*.* ";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                ExcelReader er = new(@"C:\Users\wintttr\Downloads\Test.xlsx");
+                var disciplines = new List<string>(er.GetDisciplines().Select(d => d.Name));
+
+                checkedListBox1.Items.Clear();
+                foreach (var d in disciplines)
+                    checkedListBox1.Items.Add(d);
+            }
         }
     }
 }
